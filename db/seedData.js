bc00d717a5eff31bea609dbfbaabdb8b0c48488a
jcatlanta <jcatlanta@gmail.com>
2023-02-12 13:01:39 -0500

    users.js passess all test

--- db/seedData.js
+++ db/seedData.js
@@ -1,54 +1,74 @@
 // require in the database adapter functions as you write them (createUser, createActivity...)
-// const { } = require('./');
+const { createUser } = require('./');
 const client = require("./client")
 
+
 async function dropTables() {
-  try {
   console.log("Dropping All Tables...");
+  try {
+ 
    await client.query (`
 
-      DROP TABLE IF EXIST routines_activities;
-      DROP TABLE IF EXIST routines;
-      DROP TABLE IF EXIST activities;
-      DROP TABLE IF EXIST users;
-      
+      DROP TABLE IF EXISTS routine_activities;
+      DROP TABLE IF EXISTS routines;
+      DROP TABLE IF EXISTS activities;
+      DROP TABLE IF EXISTS users;
+
     `);
     console.log("Finished dropping tables!");
   } catch (error) {
     console.error ("Error dropping tables!");
     throw error;
-  }
+  } 
 }
   // drop all tables, in the correct order
 
-
-
-async function createTables() {
-  try {
-
+ 
+  async function createTables() {
+    try {
+      console.log("Starting to build tables...");
+      // create all tables, in the correct order
   
-  console.log("Starting to build tables...");
-  await client.query(`
-  CREATE TABLE users (
-    id SERIAL PRIMARY KEY, 
-    username varchar(255)
-    UNIQUE NOT NULL, 
-    password varchar(255)
-    NOT NULL, 
-    name VARCHAR(255)
-    NOT NULL, 
-    active BOOLEAN DEFAULT true
-  );
-  `);
-    console.log("Finished building tables!");
+      await  client.query(`
+        CREATE TABLE users(
+          id  SERIAL PRIMARY KEY, 
+          username VARCHAR(255) UNIQUE NOT NULL, 
+          password VARCHAR(255) NOT NULL
+        );
+      `)
   
-} catch(error) {
-    console.error("Error building tables!");
-
-    throw error;
-}
-
-}
+      await  client.query(`
+        CREATE TABLE activities(
+          id SERIAL PRIMARY KEY, 
+          name VARCHAR(255) UNIQUE NOT NULL,
+          description TEXT NOT NULL
+        );
+      `)
+      await  client.query(`
+        CREATE TABLE routines(
+          id SERIAL PRIMARY KEY, 
+          "creatorId" INTEGER REFERENCES users(id),
+          "isPublic" BOOLEAN DEFAULT false,
+          name VARCHAR(255) UNIQUE NOT NULL,
+          goal TEXT NOT NULL
+        );
+      `)
+      await  client.query(`
+        CREATE TABLE routine_activities(
+          id SERIAL PRIMARY KEY, 
+          "routineId" INTEGER REFERENCES routines(id),
+          "activityId" INTEGER REFERENCES activities(id),
+          duration INTEGER,
+          count INTEGER,
+          UNIQUE ("routineId", "activityId")
+          );
+      `)
+      console.log("Finished building tables!");
+    } catch (error) {
+      console.error("Error building tables!");
+      throw error;
+    }
+  }
   // create all tables, in the correct order
 
 /* 
--- db/users.js
+++ db/users.js
@@ -1,23 +1,101 @@
 const client = require("./client");
+const SALT_COUNT = 10;
+const bcrypt = require('bcrypt');
+
+async function createUser ({username, password }) {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
+    const { rows: [user] } = await client.query (`
+        INSERT INTO users(username, password)
+        VALUES ($1, $2)
+        ON CONFLICT (username) DO NOTHING
+        RETURNING *;
+        `, [username, hashedPassword]);
+  
+          delete user.password
+        return user;
+      } catch (error) {
+        throw error;
+      }
+    
+  };
+
 
 // database functions
 
 // user functions
-async function createUser({ username, password }) {
-  
-}
 
 async function getUser({ username, password }) {
+  // console.log(password)
+  const user = await getUserByUsername(username);
+  // consol.log(user)
+  const hashedPassword = user.password;
+  const isValid = await bcrypt.compare(password, hashedPassword)
+  if (isValid) {
+    delete user.password
+    return user
+  }
 
 }
+
+                       
+
+// async function getUserById(userId) {
+//   // eslint-disable-next-line no-useless-catch
+//   try {
+//     const { rows: [user] } = await client.query(
+//       `SELECT id, username 
+//       WHERE id=${userId};`
+//     );  
+
+//     if (!user) {
+//       return null;
+//     }
+
+   
+//     return user;
+//   } catch (error) {
+//     throw error;
+//   }
+// }
 
 async function getUserById(userId) {
 
+  try {
+    const {rows: [ user ]} = await client.query (`
+      SELECT id, username
+      FROM users
+      WHERE id = $1;
+    `, [userId])
+
+    return user
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+
 }
 
-async function getUserByUsername(userName) {
 
+
+
+
+
+async function getUserByUsername(username) {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const { rows: [user] } = await client.query(
+      `SELECT * FROM users WHERE username=$1`,
+      [username]
+    );
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
+
+
 
 module.exports = {
   createUser,
